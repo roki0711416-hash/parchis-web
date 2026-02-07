a4d63cc8c84a9a755ca549809dc39c31ee4dfd00
あなたの名前 <your-email@example.com>
2026-02-08 00:43:48 +0900

    fix: build success and trigger production deploy

---
+++ Parchis/Parchis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Parchis
+//
+//  Created by 清水裕基 on 2026/02/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Parchis/Parchis/ParchisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParchisApp.swift
+//  Parchis
+//
+//  Created by 清水裕基 on 2026/02/08.
+//
+
+import SwiftUI
+
+@main
+struct ParchisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
